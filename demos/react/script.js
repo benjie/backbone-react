cfe856ce2b63de14db8d6920859c2fb298a4696e
Benjie Gillam <benjie@jemjie.com>
2015-01-15 15:00:49 +0000

    Add demo of React version working.

---
+++ demos/react/script.js
@@ -0,0 +1,74 @@
+var DOM = React.DOM;
+
+var HomeView = React.createClass({
+  className: "HomeView",
+
+  getInitialState: function() {
+    return {
+      randomNumber: this.randomNumber()
+    }
+  },
+
+  randomNumber: function() {
+    return Math.round(Math.random() * 100)
+  },
+
+  newNumber: function(e) {
+    this.setState({
+      randomNumber: this.randomNumber()
+    });
+  },
+
+  render: function() {
+    return DOM.div(null,
+                   DOM.h1(null, "Hello World"),
+                   DOM.p({
+                     onClick: this.newNumber
+                   }, "Random number: " + this.state.randomNumber + " (click for another)"),
+                   DOM.textarea(null, "Notes...")
+                  );
+  }
+});
+
+var AboutView = Backbone.View.extend({
+  template: _.template('<h1>About</h1><p><img src="http://www.reactiongifs.com/r/1gjdAX7.gif"></p>'),
+  render: function() {
+    this.$el.html(this.template());
+  }
+});
+
+var AppRouter = Backbone.Router.extend({
+  routes: {
+    '': 'homeRoute',
+    'about': 'aboutRoute'
+  },
+  initialize: function() {
+    this.$rootEl = $("#content");
+    this.rootEl = this.$rootEl[0];
+  },
+  setView: function(view) {
+    if (this.view) {
+      if (this.view instanceof Backbone.View) {
+        this.view.remove();
+      } else {
+        React.unmountComponentAtNode(this.rootEl);
+      }
+    }
+    this.view = view;
+    if (this.view instanceof Backbone.View) {
+      this.view.render();
+      this.$rootEl.append(this.view.el);
+    } else {
+      React.render(this.view, this.rootEl);
+    }
+  },
+  homeRoute: function() {
+    this.setView(new HomeView())
+  },
+  aboutRoute: function() {
+    this.setView(new AboutView())
+  }
+});
+
+var appRouter = new AppRouter();
+Backbone.history.start();
