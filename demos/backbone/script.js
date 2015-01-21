41143ff7bf0d6fbffd88c0d888a15ac84e59e74e
Benjie Gillam <benjie@jemjie.com>
2015-01-21 14:12:54 +0000

    Update demo code to match presentation.

--- demos/backbone/script.js
+++ demos/backbone/script.js
@@ -1,14 +1,18 @@
 var HomeView = Backbone.View.extend({
   template: _.template('<h1>Hello World!</h1><p>Random number: <span><%- number %></span> (click for another)</p><textarea>Notes...</textarea>'),
+
   events: {
     'click p': 'newNumber'
   },
+
   randomNumber: function() {
     return Math.round(Math.random() * 100)
   },
+
   newNumber: function(e) {
     this.$("span").html(this.randomNumber());
   },
+
   render: function() {
     this.$el.html(this.template({
       number: this.randomNumber()
@@ -18,20 +22,18 @@ var HomeView = Backbone.View.extend({
 
 var AboutView = Backbone.View.extend({
   template: _.template('<h1>About</h1><p><img src="http://www.reactiongifs.com/r/1gjdAX7.gif"></p>'),
+
   render: function() {
     this.$el.html(this.template());
   }
 });
 
 var AppRouter = Backbone.Router.extend({
-  routes: {
-    '': 'homeRoute',
-    'about': 'aboutRoute'
-  },
   initialize: function() {
     this.$rootEl = $("#content");
     this.rootEl = this.$rootEl[0];
   },
+
   setView: function(view) {
     if (this.view) {
       this.view.remove();
@@ -40,9 +42,18 @@ var AppRouter = Backbone.Router.extend({
     this.view.render();
     this.$rootEl.append(this.view.el);
   },
+
+  // ----------
+
+  routes: {
+    '': 'homeRoute',
+    'about': 'aboutRoute'
+  },
+
   homeRoute: function() {
     this.setView(new HomeView())
   },
+
   aboutRoute: function() {
     this.setView(new AboutView())
   }
--- demos/react/script.js
+++ demos/react/script.js
@@ -1,7 +1,7 @@
 var DOM = React.DOM;
 
 var HomeView = React.createClass({
-  className: "HomeView",
+  displayName: "HomeView",
 
   getInitialState: function() {
     return {
@@ -21,31 +21,29 @@ var HomeView = React.createClass({
 
   render: function() {
     return DOM.div(null,
-                   DOM.h1(null, "Hello World"),
-                   DOM.p({
-                     onClick: this.newNumber
-                   }, "Random number: " + this.state.randomNumber + " (click for another)"),
-                   DOM.textarea(null, "Notes...")
-                  );
+      DOM.h1(null, "Hello World"),
+      DOM.p({
+        onClick: this.newNumber
+      }, "Random number: " + this.state.randomNumber + " (click for another)"),
+      DOM.textarea(null, "Notes...")
+    );
   }
 });
 
 var AboutView = Backbone.View.extend({
   template: _.template('<h1>About</h1><p><img src="http://www.reactiongifs.com/r/1gjdAX7.gif"></p>'),
+
   render: function() {
     this.$el.html(this.template());
   }
 });
 
 var AppRouter = Backbone.Router.extend({
-  routes: {
-    '': 'homeRoute',
-    'about': 'aboutRoute'
-  },
   initialize: function() {
     this.$rootEl = $("#content");
     this.rootEl = this.$rootEl[0];
   },
+
   setView: function(view) {
     if (this.view) {
       if (this.view instanceof Backbone.View) {
@@ -62,9 +60,18 @@ var AppRouter = Backbone.Router.extend({
       React.render(this.view, this.rootEl);
     }
   },
+
+  // ----------
+
+  routes: {
+    '': 'homeRoute',
+    'about': 'aboutRoute'
+  },
+
   homeRoute: function() {
     this.setView(new HomeView())
   },
+
   aboutRoute: function() {
     this.setView(new AboutView())
   }
